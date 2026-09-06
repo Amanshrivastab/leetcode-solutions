44ce81495840fa1e1c7166e8316403ea52ab7cd2
Aman shrivastab <akshri2007@gmail.com>
2026-09-06 23:38:33 +0530

    ✅ Longest Substring Without Repeating Characters | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-09-06

--- medium/0003-longest-substring-without-repeating-characters/solution.cpp
+++ medium/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,14 +1,12 @@
-        int result = 0;
-
-        for (int right = 0; right<s.length(); right++){
-            if(st.find(s[right]) !=st.end() ){
                 st.erase(s[left]);
                 left++;
             }
-        }
-        
-    }
+
             st.insert(s[right]);
-            result = max(result,right - left +1);
-        return result ;
+
+            result = max(result, right - left + 1);
+        }
+
+        return result;
+    }
 };
