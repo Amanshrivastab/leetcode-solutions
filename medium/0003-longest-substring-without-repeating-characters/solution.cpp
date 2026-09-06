e4c8f58e00945e89c2534e32a022808a0157caee
Aman shrivastab <akshri2007@gmail.com>
2026-09-06 23:18:20 +0530

    ✅ Longest Substring Without Repeating Characters | Medium | C++ | Runtime: 0 ms | Memory: 7.8 MB | 2026-09-06

---
+++ medium/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,14 @@
+        int arr[strlen];
+        int result=0;
+        for(int i=0; i<strlen;i++){
+        
+            if(arr[i]==s[i]){
+        }
+                continue;
+            }
+            else{
+                result = result+1;
+            }
+    }
+        return result;
+};
