347d3168c0546a61c2e714a066bb8c6f96f0a623
Aman shrivastab <akshri2007@gmail.com>
2026-09-13 23:59:09 +0530

    ✅ Integer to Roman | Medium | C++ | Runtime: 7 ms (28.61%) | Memory: 12.2 MB | 2026-09-13

--- medium/0012-integer-to-roman/solution.cpp
+++ medium/0012-integer-to-roman/solution.cpp
@@ -1,10 +1,29 @@
+class Solution {
+public:
+    string intToRoman(int num) {
 
-            while (num >= values[i]) {
-                res += symbols[i];
-                num -= values[i];
-            }
-        }
+        vector<int> values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
 
-        return res;
-    }
+        vector<string> symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        string res = "";
+
+        for (int i = 0; i < values.size(); i++) {
+
+            while (num >= values[i]) {
+                res += symbols[i];
+                num -= values[i];
+            }
+        }
+
+        return res;
+    }
 };
