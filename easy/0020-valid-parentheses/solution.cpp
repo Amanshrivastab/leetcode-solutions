e048901e9607cfc1222b18e2ae2ce3e86c0803dc
Aman shrivastab <akshri2007@gmail.com>
2026-09-21 05:47:59 +0530

    ✅ Valid Parentheses | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.9 MB | 2026-09-21

---
+++ easy/0020-valid-parentheses/solution.cpp
@@ -0,0 +1,40 @@
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+
+            // Opening brackets
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+
+            // Closing brackets
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                if (c == ')' && st.top() != '(') {
+                    return false;
+                }
+
+                if (c == ']' && st.top() != '[') {
+                    return false;
+                }
+
+                if (c == '}' && st.top() != '{') {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
